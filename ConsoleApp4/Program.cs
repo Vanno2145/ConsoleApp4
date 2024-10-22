9df40c370c3bfedd272cb00d8ccebfaec4ed0228
ivanm <ivanm@DESKTOP-F33GBDN>
2024-10-22 19:00:35 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+
+namespace ConsoleApp4
+{
+    class Card
+    {
+        public long number { get; set; }
+        public string FIO { get; set; }
+        public int CVC { get; set; }
+        public int stopDate { get; set; }
+
+        public Card()
+        {
+            number = 0;
+            FIO = " ";
+            CVC = 0;
+            stopDate = 0;
+        }
+        public void Input(long number, string FIO, int CVC, int stopDate)
+        {
+            try
+            {
+                this.number = number;
+                this.FIO = FIO;
+                if (CVC < 100 || CVC > 999) 
+                {
+                    throw new Exception("Incorrect CVC");
+                }
+                else
+                {
+                    this.CVC = CVC;
+                }
+                this.stopDate = stopDate;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Number: {number}\nFIO: {FIO}\nCVC: {CVC}\nStopdate: {stopDate}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card();
+            card.Input(4024007123874108, "Ivan Mlanar", 431, 0);
+            card.Print();
+        }
+    }
+}
